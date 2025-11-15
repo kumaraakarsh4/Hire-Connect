3130829de04088f781d74377e9ac2408a586dd01
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-16 01:08:17 +0530

    code updation fix

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express()
-app.get("/" , (req,res)=>{
-    res.status(200).json({msg:"Success from backend"})
-
-})
-app.listen(3000,()=>console.log("server is running on 3000")
-);
