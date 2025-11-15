52be9a8c4ff3e940a772708e872bc41391e2abc9
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-16 00:27:47 +0530

    code updation

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("hey! Aakarsh server");
+import express from "express";
+const app = exprees()
+app.get("/" , (req,res)=>{
+    res.status(200).json({msg:"Success from api"})
+
+})
+app.listen(3000,()=>console.log("server is running on 3000")
+);
