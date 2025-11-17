1744db6fa50c785237bc43b2fab0d8ff2076f032
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-17 16:11:49 +0530

    code fixes connecting to db

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,14 +1,14 @@
-import mongoose, { connection } from "mongoose";
+import mongoose  from "mongoose";
 import { ENV } from "./env.js";
 
 export const connectDB = async()=>{
     try {
-        await mongoose.connect(ENV.DB_URL)
-        console.log(" ☑️ connected to MongoDB:",connectDB,connection.host);
+       const conn =  await mongoose.connect(ENV.DB_URL);
+        console.log(" ☑️ connected to MongoDB:",conn.connection.host);
         
         
     } catch (error) {
         console.error(" ❌ Error connecting to MongoDB",error)
-        process.exit(1) // 0 means success, 1 means failure
+        process.exit(1); // 0 means success, 1 means failure
     }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+
 const app = express()
 console.log(ENV.PORT);
 console.log(ENV.DB_URL);
@@ -9,5 +11,21 @@ app.get("/" , (req,res)=>{
     res.status(200).json({msg:"Success from backend123647"})
 
 })
-app.listen(ENV.PORT,()=>console.log("server is running on port:",ENV.PORT)
-);
+
+
+const startserver = async ()=>{
+    try {
+        await connectDB();
+        app.listen(ENV.PORT,()=>{
+console.log("Server is running on port:" ,ENV.PORT);
+
+});
+        
+        
+    } catch (error) {
+        console.error(" ❌ Error connecting to server" ,error)
+        
+    }
+}
+
+startserver();
