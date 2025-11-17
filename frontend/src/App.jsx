c2da561685049f223f5d4f4af888c6fe274ac555
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-17 18:10:39 +0530

    code updation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
-
-import { SignInButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, UserButton, SignOutButton } from '@clerk/clerk-react'
 import './App.css'
 
 function App() {
   
-
   return (
     <>
-      
       <h1>Welcome to the server! </h1>
-      <SignInButton mode='modal'/>
+
+      {/* 1. SHOW WHEN SIGNED OUT */}
+      <SignedOut>
+        {/* The built-in Clerk component to open the sign-in modal */}
+        <SignInButton mode='modal'> 
+            {/* You can wrap a custom button/text with SignInButton */}
+            <button className='ankhdh'>Sign in please</button>
+        </SignInButton>
+        {/* The SignIn component below is usually unnecessary unless you want the full-page sign-in form */}
+      </SignedOut>
+
+      {/* 2. SHOW WHEN SIGNED IN */}
+      <SignedIn>
+        {/* The standard component to display user profile and Sign Out */}
+        <UserButton /> 
+      </SignedIn>
       
-          </>
+    </>
   )
 }
 
