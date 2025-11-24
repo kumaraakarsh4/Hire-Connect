b290186e12f10793edd69383381bda6f12158cf7
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-25 01:01:33 +0530

    session route code update

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -6,4 +6,6 @@ router.post("/" , protectRoute,createSession);
 router.get("/Active" ,  protectRoute, getActiveSession);
 router.get("/my-recent" ,  protectRoute, getMyRecentSession);
 router.get("/:id" ,  protectRoute, getSessionById);
+router.post("/:id/join" ,  protectRoute, joinSession);
+router.post("/:id/end" ,  protectRoute, endSession);
 export default router;
