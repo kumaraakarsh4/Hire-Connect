a314a3c42ac897143ce992f65c55e468e1bfc9de
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-24 17:21:49 +0530

    chat route and controller code updated

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { getStreamToken } from '../controllers/chatController.js';
+import { protectRoute } from '../middleware/protectRoutes.js';
 
 const router = express.router();
 
-router.get("/token" , getStreamToken)
+router.get("/token" , protectRoute,getStreamToken)
 export default router;
