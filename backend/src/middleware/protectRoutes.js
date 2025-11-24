7563c2614973aa0400636715f513a19753bfbfa7
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-24 14:42:15 +0530

    protectRoute code updation

--- backend/src/middleware/protectRoutes.js
+++ backend/src/middleware/protectRoutes.js
@@ -5,6 +5,11 @@ export const protectRoute=[
     async (req,res,next)=>{
         try {
             const clerkId= req.auth().userId;
+            if(!clerkId) return res.status(401).json({msg:"unauthorized access - token"});
+            const user = await user.findOne({clerkId});
+            if(!user) return res.status(404).json({msg:"User not Found"});
+            req.user = user;
+            next();
         } catch (error) {
             
         }
