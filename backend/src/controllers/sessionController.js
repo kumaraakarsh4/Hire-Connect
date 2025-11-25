986b97540fbfd01f4b27e32b492c1156bc7009b8
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-25 14:32:53 +0530

    session controller code updation

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -1,5 +1,19 @@
+import { Session } from "@clerk/express";
+
 export async function createSession(req,res) {
+  try {
+    const {problem,diffculty} = req.body;
+    const userId = req.user._id;
+    const clerkId = req.user.clerkId;
+    if(!problem || !diffculty){
+        return res.status(400).json({msg:"problem and diffculty required"});
+    }
+    const callId = `session_${Date.now()}_${Math.random().toString(36).substring(7)}`;
+    const session = await Session.create({problem,diffculty,host: userId , callId});
     
+  } catch (error) {
+    
+  }  
 }
 export async function getActiveSession(req,res) {
     
