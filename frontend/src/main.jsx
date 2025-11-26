32d533e46d0629fccf61f91a93349d5a5542df00
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-11-26 17:06:46 +0530

    updating my code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import {BrowserRouter} from 'react-router';
+import {
+  QueryClient,
+  QueryClientProvider,
+  
+} from '@tanstack/react-query'
+
 
 
 // Import your Publishable Key
@@ -13,14 +19,18 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing clerk Publishable Key')
 }
 
+const queryClient = new QueryClient()
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
         <App />
       </ClerkProvider>
+      </QueryClientProvider>
     </BrowserRouter>
     
   </StrictMode>,
