8cdc0ccee9820facc21f32247fcbacea42b3b56d
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-12-02 01:07:28 +0530

    code updation and fixing some errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
 import { Toaster } from 'react-hot-toast';
 import DashboardPage from './pages/DashboardPage';
+import ProblemPage from './pages/ProblemsPage';
 
 function App() {
   const {isSignedIn,isLoaded}=useUser();
@@ -16,7 +17,9 @@ function App() {
      <Route path="/" element={!isSignedIn ? <HomePage/> :  <Navigate to={"/dashboard"}/> }/>
      <Route path="/dashboard" element={!isSignedIn ? <DashboardPage/> :  <Navigate to={"/"}/> }/>
     
-       <Route path="/problem" element={ <ProblemsPage/> }/>
+       <Route path="/problem" element={!isSignedIn ? <ProblemsPage/>  :  <Navigate to={"/"}/> }/>
+        <Route path="/problem:id" element={!isSignedIn ? <ProblemPage/>  :  <Navigate to={"/"}/> }/>
+
 
       
       
