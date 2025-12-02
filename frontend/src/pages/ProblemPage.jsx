7ac98461ec919514e4d5c6f5562d10e2fa07737e
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-12-03 00:37:51 +0530

    code updation of problem page

--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { PROBLEMS } from "../data/problems";
 function ProblemPage(){
     const {id}= useParams();
     const navigate = useNavigate();
 
-    const [currentProblemId,setCurrentProblemId] = useState("two-sum")
-     const [selectedLanguage,setselectedLanguage] = useState("javascript")
-      const [code,setcode] = useState(PROBLEMS[currentProblemId].starterCode.javascript)
+    const [currentProblemId,setCurrentProblemId] = useState("two-sum");
+     const [selectedLanguage,setselectedLanguage] = useState("javascript");
+      const [code,setcode] = useState(PROBLEMS[currentProblemId].starterCode.javascript);
+       const [output, setOutput] = useState(null);
+        const [isRunning, setIsRunning] = useState(false);
+
+      
+
+        const currentProblem = PROBLEMS[currentProblemId]
+        // update problem when url param changes
+        useEffect(()=>{
+          if(id&&PROBLEMS[id]){
+            setCurrentProblemId(id)
+            setcode(PROBLEMS[id].starterCode[selectedLanguage])
+            setOutput(null)
+          }
+        },[id,selectedLanguage])
+
+        const handleProblemChange= (e)=>{}
+        const handleProblemChange = ()=>{}
+
+        const triggerConfett=()=>{}
+
+        const checkIfTestsPassed=()=>{}
+
+        const handleRunCode=()=>{}
     return
     <div>ProblemPage</div>;
 }
