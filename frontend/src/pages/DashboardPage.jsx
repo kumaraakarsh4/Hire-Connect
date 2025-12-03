c8df1f7cb90bcaf7a03af582d00c06f7915b0a35
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-12-03 17:13:07 +0530

    dashboard page code updation

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router';
+import {useUser} from '@clerk/clerk-react';
+import { useActiveSessions, useCreateSession, useMyRecentSessions } from '../hooks/useSession';
 
 function DashboardPage() {
+  const navigate = useNavigate()
+  const {user} = useUser()
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [roomConfig, setRoomConfig] = useState({ problem: "", difficulty: "" });
+ const useCreateSessionMutation= useCreateSession()
+ const {data:activeSessionsData,isLoading:loadingActiveSessions} = useActiveSessions();
+ const {data:recentSessionData, isLoading:loadingRecentSessions}= useMyRecentSessions()
+
   return (
     <div>DashboardPage</div>
   )
