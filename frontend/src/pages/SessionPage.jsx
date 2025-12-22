f00f1d4e458ce75bf9fc3b10b422f169cbf28ea3
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-12-23 00:29:33 +0530

    code updation

--- frontend/src/pages/SessionPage.jsx
+++ frontend/src/pages/SessionPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useUser } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
 
 function SessionPage() {
   return (
