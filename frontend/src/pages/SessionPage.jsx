18591d6e172be3dbde34183a8505e1b8b4ecd75f
kumaraakarsh4 <106160870+kumaraakarsh4@users.noreply.github.com>
2025-12-30 23:19:35 +0530

    code update

---
+++ frontend/src/pages/SessionPage.jsx
@@ -0,0 +1,6 @@
+import { useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { useEndSession, useJoinSession, useSessionById } from "../hooks/useSessions";
+import { PROBLEMS } from "../data/problems";
+import { executeCode } from "../lib/piston";
